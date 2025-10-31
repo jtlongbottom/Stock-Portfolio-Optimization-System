8526313f2305e2024cc00e16e63b6e651b2d5202
jtlongbottom <107249668+jtlongbottom@users.noreply.github.com>
2025-10-30 22:16:03 -0700

    small version of data grabbing for testing

---
+++ helper.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 29 21:00:27 2025
+
+@author: jakoblongbottom
+"""
+
+import yfinance as yf
+
+import pandas as pd
+
+
+df = pd.read_csv('top1000_assets_ranked_by_sharpe_ratio.csv')
+
+
+
+
+tickers = df['asset'][:200]
+
+
+
+
+def get_stock_names (tickers):
+    names = []
+    
+    for ticker in tickers:
+        info = yf.Ticker(ticker).info
+        if 'shortName' in list(info.keys()):
+            name = info['shortName']
+        #asset_type = info['typeDisp']
+            
+        elif 'longName' in list(info.keys()):
+            name = info['longName']
+        names.append(name)
+    return names
+        
